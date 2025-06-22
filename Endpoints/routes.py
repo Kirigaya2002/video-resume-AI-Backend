e9fd371ee81804a2b04e30b2d373a5c5dd1aede6
DennisUCR <DENNIS.GOMEZ@UCR.AC.CR>
2025-06-22 16:53:43 -0600

    extractor de audio

--- Endpoints/routes.py
+++ Endpoints/routes.py
@@ -2,20 +2,25 @@ from flask import Blueprint, jsonify
 from Services import *
 from Services import resume_audio
 from Services import video_decoder
+from Services import audio_extract
+from Services import transcript_audio
 
 routes = Blueprint("routes", __name__)
-
 
 @routes.get("/process-video")
 def process_video(Video: str):
     # Pasar video de base 64 a mp4
     video_decoder.decode_video(Video)
+
     # Extraer audio del video
-    audio_text = ""
+    audio_extract.audio_extract("video.mp4", "audio.mp3")
+
     # Transcribir audio a texto
     transcription = transcript_audio.transcript_audio()
+
     # Resumir texto obtenido
     resumed_text = resume_audio.resume_audio(transcription)
+    
     # crear un json con la transcripcion y el resumen
     json_res = {"transcription": transcription, "resumed_text": resumed_text}
     return jsonify(json_res)
